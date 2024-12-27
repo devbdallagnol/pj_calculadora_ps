4a43c8d2dd2304f8a1c6169fd9a9ef8afcbdd511
Bruno DallAgnol <brunomorpheu@gmail.com>
2024-12-26 22:23:18 -0300

    added - criando displayContainer

--- src/components/Displays/Display.js
+++ src/components/Displays/Display.js
@@ -6,14 +6,13 @@ const DisplayContainer = styled.div`
   background-color: #1e1e1e;
   color: #ffffff;
   font-size: 2em;
-  padding: 20px;
-  margin: 20px 0;
+  height: 70vh;
+  padding: 10px;
+  margin: 50px 0;
   border-radius: 10px;
   border: 3px solid #c1c1c1;
   box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
   display: flex;
-  justify-content: center;
-  align-items: center;
   transition: background-color 0.3s ease-in-out;
 
   &:hover {
